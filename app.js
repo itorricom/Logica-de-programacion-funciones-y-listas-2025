1848392147005395a9d716e92b2ea5c1eed33db5
Isidoro Torrico M <itorrico.m@gmail.com>
2025-02-27 22:35:41 -0400

    Asigna texto a un elemento HTML

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del numero secreto';
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un numero del 1 al 10';
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
 
 function intentoDeUsuario(){
     alert('Clic desde el boton');
 }
+
+asignarTextoElemento('h1','Juego del numero secreto!');
+asignarTextoElemento('p','Indica un numero del 1 al 10');
