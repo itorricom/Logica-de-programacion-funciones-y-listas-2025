63faed28f7d470ca75ac06f218f5e110ad0fc8bd
Isidoro Torrico M <itorrico.m@gmail.com>
2025-02-27 22:52:21 -0400

    Genera un número aleatorio

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto);
+
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
@@ -5,6 +8,10 @@ function asignarTextoElemento(elemento, texto){
 
 function intentoDeUsuario(){
     alert('Clic desde el boton');
+}
+
+function generarNumeroSecreto(){
+    return Math.floor(Math.random() * 10) + 1;
 }
 
 asignarTextoElemento('h1','Juego del numero secreto!');
