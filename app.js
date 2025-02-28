958967d36e8822d3b03b8e1855388daa31942c14
Isidoro Torrico M <itorrico.m@gmail.com>
2025-02-27 23:45:15 -0400

    Mensaje, si acertó

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 let numeroSecreto = generarNumeroSecreto();
 console.log(numeroSecreto);
 
-function asignarTextoElemento(elemento, texto){
+function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
 }
 
-function verificarIntento(){
+function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
-    console.log(typeof(numeroDeUsuario));
-    console.log(numeroDeUsuario);  
-    console.log(typeof(numeroSecreto));
     console.log(numeroSecreto);
-    console.log(numeroDeUsuario===numeroSecreto);
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', 'Acertaste el número');
+    }else{
+        if(numeroDeUsuario>numeroSecreto){
+            asignarTextoElemento('p', 'El número secreto es menor');
+        }else{
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+    }
     return;
 }
 
-function generarNumeroSecreto(){
+function generarNumeroSecreto() {
     return Math.floor(Math.random() * 10) + 1;
 }
 
-asignarTextoElemento('h1','Juego del numero secreto!');
-asignarTextoElemento('p','Indica un numero del 1 al 10');
+asignarTextoElemento('h1', 'Juego del numero secreto!');
+asignarTextoElemento('p', 'Indica un numero del 1 al 10');
